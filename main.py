a98f975915286ddb8cd22d0642ad00af35dd6f8c
Johannes Bausch <johannes.bausch@gmail.com>
2020-05-01 18:20:38 +0100

    implemented checkpointing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from typing import List, Union
 
-import os
+import os, time
 import torch
 import torch.nn as nn
 from torch.nn.parallel import DistributedDataParallel
@@ -17,15 +17,19 @@ from math import pi
 
 from termcolor import colored
 
+import secrets
+
 
 class DistributedTrainingEnvironment:
-    def __init__(self, shard: int, world_size: int, port: int):
+    def __init__(self, shard: int, args):
         self.shard = shard
-        self.world_size = world_size
-        self.port = port
+        self.world_size = args.num_shards
+        self.port = args.port
+        self._original_args = args
+        self._checkpoint_prefix = secrets.token_hex(3)  # these are different in different shards; so checkpoint from the same shard always
 
         print(
-            f"[{shard}] Hello from shard {shard} in a world of size {world_size}! Happy training!"
+            f"[{shard}] Hello from shard {shard} in a world of size {self.world_size}! Happy training!"
         )
 
     def __enter__(self):
@@ -69,40 +73,93 @@ class DistributedTrainingEnvironment:
         return gather_list
 
 
+    CHECKPOINT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "checkpoints/")
+
+    def save_checkpoint(self, model, optimizer, **kwargs):
+        """
+            we don't check whether this is only called from shard 0
+        """
+        kwargs.update({
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "_original_args": self._original_args,
+            "_checkpoint_prefix": self._checkpoint_prefix
+        })
+
+        filename = f"checkpoint-{self._checkpoint_prefix}-" + time.strftime("%Y-%m-%d--%H-%M-%S") + ".tar"
+        path = os.path.join(self.CHECKPOINT_PATH, filename)
+
+        torch.save(kwargs, path)
+        return filename
+
+
+    def load_checkpoint(self, path: str) -> dict:
+        store = torch.load(path)
+        self._original_args = store["_original_args"]
+        self._checkpoint_prefix = store["_checkpoint_prefix"]
+        return store
+
+
 def train(shard: int, args):
-    with DistributedTrainingEnvironment(shard, args.num_shards, args.port) as environment:
+    with DistributedTrainingEnvironment(shard, args) as environment:
         print, print_all = environment.print_once, environment.print_all
 
         EPOCHS = 10000
+        RESUME_MODE = hasattr(args, "filename")
 
-        if args.dataset == "simple":
-            dataset = datasets.DataSimpleSentences(shard, **vars(args))
-        elif args.dataset == "elman-xor":
-            dataset =  datasets.DataElmanXOR(shard, **vars(args))
-        elif args.dataset == "elman-letter":
-            dataset =  datasets.DataElmanLetter(shard, **vars(args))
-        elif args.dataset == "shakespeare":
-            dataset =  datasets.DataShakespeare(**vars(args))
+        # either load or initialize new
+        if RESUME_MODE:
+            store = environment.load_checkpoint(args.filename)
+            original_args = store["_original_args"]
+            epoch_start = store["epoch"]
+            best_validation_loss = store["best_validation_loss"]
+
+        else:
+            original_args = args
+            epoch_start = 0
+            best_validation_loss = 10.**10
+
+
+        if original_args.dataset == "simple":
+            dataset = datasets.DataSimpleSentences(shard, **vars(original_args))
+        elif original_args.dataset == "elman-xor":
+            dataset =  datasets.DataElmanXOR(shard, **vars(original_args))
+        elif original_args.dataset == "elman-letter":
+            dataset =  datasets.DataElmanLetter(shard, **vars(original_args))
+        elif original_args.dataset == "shakespeare":
+            dataset =  datasets.DataShakespeare(**vars(original_args))
 
         # create model and distribute
-        rvqe = RVQE(workspace_size=args.workspace, stages=args.stages, order=args.order)
+        rvqe = RVQE(workspace_size=original_args.workspace, stages=original_args.stages, order=original_args.order)
         rvqe_ddp = DistributedDataParallel(rvqe)
 
-        for p in rvqe_ddp.parameters():
-            nn.init.uniform_(p, a=0.0, b=2 * pi)
+        # create optimizer
+        if original_args.optimizer == "adam":
+            optimizer = torch.optim.Adam(rvqe_ddp.parameters(), lr=original_args.learning_rate)
+        elif original_args.optimizer == "rmsprop":
+            optimizer = torch.optim.RMSprop(rvqe_ddp.parameters(), lr=original_args.learning_rate)
 
-        print(f"model has {len(list(rvqe_ddp.parameters()))} parameters.")
+        # when in resume mode, load model and optimizer state; otherwise initialize
+        if RESUME_MODE:
+            rvqe_ddp.load_state_dict(store["model_state_dict"])
+            optimizer.load_state_dict(store["optimizer_state_dict"])
+        else:
+            for p in rvqe_ddp.parameters():
+                nn.init.uniform_(p, a=0.0, b=2 * pi)
 
-        postselected_training_phase = True
-        if args.optimizer == "adam":
-            optimizer = torch.optim.Adam(rvqe_ddp.parameters(), lr=args.learning_rate)
-        elif args.optimizer == "rmsprop":
-            optimizer = torch.optim.RMSprop(rvqe_ddp.parameters(), lr=args.learning_rate)
-
+        # cross entropy loss
         criterion = nn.CrossEntropyLoss()
 
+        # wait for all shards to be happy
+        environment.synchronize()
+
+        if RESUME_MODE:
+            print(f"⏩ Resuming session! Model has {len(list(rvqe_ddp.parameters()))} parameters, and we start at epoch {epoch_start} with best validation loss {best_validation_loss:7.3e}.")
+        else:
+            print(f"⏩ New session! Model has {len(list(rvqe_ddp.parameters()))} parameters.")
+
         time_start = timer()
-        for epoch in range(EPOCHS):
+        for epoch in range(epoch_start, EPOCHS):
             epoch += 1
             time_start = timer()
 
@@ -121,7 +178,7 @@ def train(shard: int, args):
             # print loss each few epochs
             if epoch % 1 == 0:
                 print(
-                    f"{'PS' if postselected_training_phase else 'MS'} {epoch:04d}/{EPOCHS:04d} {timer() - time_start:5.1f}s  loss={loss:7.3e}"
+                    f"{epoch:04d}/{EPOCHS:04d} {timer() - time_start:5.1f}s  loss={loss:7.3e}"
                 )
 
             # print samples every few epochs
@@ -153,70 +210,20 @@ def train(shard: int, args):
                         validation_loss /= args.num_shards
                         print(colored(f"validation loss: {validation_loss:7.3e}", "green"))
 
+                        # checkpointing
+                        if validation_loss < best_validation_loss:
+                            best_validation_loss = validation_loss
+                            succ = environment.save_checkpoint(rvqe_ddp, optimizer, **{
+                                "epoch": epoch,
+                                "best_validation_loss": best_validation_loss
+                            })
+                            if succ is not None:
+                                print(colored(f"saved new best checkpoint {succ}", "green"))
+
                 environment.synchronize()
 
 
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(
-        description="RVQE Training Script", formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument(
-        "--workspace", metavar="W", type=int, default=5, help="qubits to use as workspace",
-    )
-    parser.add_argument("--stages", metavar="S", type=int, default=2, help="RVQE cell stages")
-    parser.add_argument(
-        "--order", metavar="O", type=int, default=2, help="order of activation function"
-    )
-    parser.add_argument(
-        "--port", metavar="P", type=int, default=12335, help="port for distributed computing",
-    )
-    parser.add_argument(
-        "--num-shards",
-        metavar="N",
-        type=int,
-        default=2,
-        help="number of cores to use for parallel processing",
-    )
-    parser.add_argument(
-        "--dataset",
-        metavar="D",
-        type=str,
-        default="simple",
-        help="dataset; choose between simple, xor and shakespeare",
-    )
-    parser.add_argument(
-        "--sentence-length",
-        metavar="SL",
-        type=int,
-        default=20,
-        help="sentence length for data generators",
-    )
-    parser.add_argument(
-        "--batch-size",
-        metavar="B",
-        type=int,
-        default=1,
-        help="batch size; only relevant for shakespeare dataset",
-    )
-    parser.add_argument(
-        "--optimizer",
-        metavar="OPT",
-        type=str,
-        default="adam",
-        help="optimizer; one of adam or rmsprop",
-    )
-    parser.add_argument(
-        "--learning-rate",
-        metavar="LR",
-        type=float,
-        default="0.003",
-        help="learning rate for optimizer",
-    )
-
-    args = parser.parse_args()
-
+def command_train(args):
     # validate
     required_workspace = {"simple": 3, "elman-xor": 1, "elman-letter": 6, "shakespeare": 5}
     assert args.dataset in required_workspace, "invalid dataset"
@@ -226,3 +233,88 @@ if __name__ == "__main__":
     assert args.optimizer in ["adam", "rmsprop"], "invalid optimizer"
 
     torch.multiprocessing.spawn(train, args=(args,), nprocs=args.num_shards, join=True)
+
+
+def command_resume(args):
+    torch.multiprocessing.spawn(train, args=(args,), nprocs=args.num_shards, join=True)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="RVQE Training Script", formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    subparsers = parser.add_subparsers(help="available commands")
+
+    parser_train = subparsers.add_parser("train")
+    parser_train.set_defaults(func=command_train)
+    parser_train.add_argument(
+        "--workspace", metavar="W", type=int, default=5, help="qubits to use as workspace",
+    )
+    parser_train.add_argument("--stages", metavar="S", type=int, default=2, help="RVQE cell stages")
+    parser_train.add_argument(
+        "--order", metavar="O", type=int, default=2, help="order of activation function"
+    )
+    parser_train.add_argument(
+        "--port", metavar="P", type=int, default=12335, help="port for distributed computing",
+    )
+    parser_train.add_argument(
+        "--num-shards",
+        metavar="N",
+        type=int,
+        default=2,
+        help="number of cores to use for parallel processing",
+    )
+    parser_train.add_argument(
+        "--dataset",
+        metavar="D",
+        type=str,
+        default="simple",
+        help="dataset; choose between simple, xor and shakespeare",
+    )
+    parser_train.add_argument(
+        "--sentence-length",
+        metavar="SL",
+        type=int,
+        default=20,
+        help="sentence length for data generators",
+    )
+    parser_train.add_argument(
+        "--batch-size",
+        metavar="B",
+        type=int,
+        default=1,
+        help="batch size; only relevant for shakespeare dataset",
+    )
+    parser_train.add_argument(
+        "--optimizer",
+        metavar="OPT",
+        type=str,
+        default="adam",
+        help="optimizer; one of adam or rmsprop",
+    )
+    parser_train.add_argument(
+        "--learning-rate",
+        metavar="LR",
+        type=float,
+        default="0.003",
+        help="learning rate for optimizer",
+    )
+
+    parser_resume = subparsers.add_parser("resume")
+    parser_resume.set_defaults(func=command_resume)
+    parser_resume.add_argument("filename", type=str, help="checkpoint filename")
+    parser_resume.add_argument(
+        "--port", metavar="P", type=int, default=12335, help="port for distributed computing",
+    )
+    parser_resume.add_argument(
+        "--num-shards",
+        metavar="N",
+        type=int,
+        default=2,
+        help="number of cores to use for parallel processing",
+    )
+
+    args = parser.parse_args()
+    args.func(args)

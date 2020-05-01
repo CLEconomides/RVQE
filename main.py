33839985527d75dd1a70f6ecaf843ae27f65cabf
Johannes Bausch <johannes.bausch@gmail.com>
2020-05-01 15:55:32 +0100

    added joint validation output

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+from typing import List, Union
+
 import os
 import torch
 import torch.nn as nn
 from torch.nn.parallel import DistributedDataParallel
+from torch.distributed import ReduceOp
 from timeit import default_timer as timer
 
 from RVQE.model import RVQE
@@ -12,7 +15,7 @@ from RVQE import datasets
 
 from math import pi
 
-from termcolor import cprint
+from termcolor import colored
 
 
 class DistributedTrainingEnvironment:
@@ -45,10 +48,30 @@ class DistributedTrainingEnvironment:
         if self.shard == 0:
             print(*args, **kwargs)
 
+    def print_all(self, *args, **kwargs):
+        print(f"[{self.shard}]", *args, **kwargs)
+
+    def synchronize(self):
+        return torch.distributed.barrier()
+
+    def reduce(self, data: tensor, reduce_op: ReduceOp) -> tensor:
+        torch.distributed.reduce(data, 0, reduce_op)
+        return data
+
+    def gather(self, data: tensor) -> List[tensor]:
+        gather_list = None
+        if self.shard == 0:
+            gather_list = []
+            for _ in range(self.world_size):
+                gather_list.append(torch.zeros_like(data))
+
+        torch.distributed.gather(data, gather_list, 0)
+        return gather_list
+
 
 def train(shard: int, args):
     with DistributedTrainingEnvironment(shard, args.num_shards, args.port) as environment:
-        print = environment.print_once
+        print, print_all = environment.print_once, environment.print_all
 
         EPOCHS = 10000
 
@@ -104,17 +127,33 @@ def train(shard: int, args):
             # print samples every few epochs
             if epoch % 10 == 0:
                 with torch.no_grad():
-                    for sentence in sentences:
-                        _, measured_seq = rvqe(sentence, postselect_measurement=False)
-                        cprint(
-                            f"[{shard}] gold = { dataset.to_human(sentence) }", "yellow",
-                        )
-                        cprint(
-                            f"[{shard}] pred = { dataset.to_human(measured_seq, offset=1) }",
-                            "white",
-                        )
+                    sentence, target = sentences[0], targets[0]
+                    measured_probs, measured_seq = rvqe(sentence, postselect_measurement=False)
+                    validation_loss = criterion(measured_probs, target[1:])
+                    
+                    # collect in main shard
+                    sentences = environment.gather(sentence)
+                    measured_seqs = environment.gather(measured_seq)
+                    validation_loss = environment.reduce(validation_loss, ReduceOp.SUM)
 
-                        break  # only show one for now
+                    if shard == 0:
+                        assert len(measured_seqs) == args.num_shards, "gather failed somehow"
+
+                        for i in range(min(3, args.num_shards)):
+                            seq = measured_seqs[i]
+                            sen = sentences[i]
+
+                            print(
+                                colored(f"gold = { dataset.to_human(sen) }", "yellow"),
+                            )
+                            print(
+                                f"pred = { dataset.to_human(seq, offset=1) }"
+                            )
+
+                        validation_loss /= args.num_shards
+                        print(colored(f"validation loss: {validation_loss:7.3e}", "green"))
+
+                environment.synchronize()
 
 
 if __name__ == "__main__":
@@ -183,7 +222,7 @@ if __name__ == "__main__":
     assert args.dataset in required_workspace, "invalid dataset"
     assert (
         required_workspace[args.dataset] < args.workspace
-    ), f"need a workspace of size {required_workspace[args.dataset]} for {args.dataset} dataset"
+    ), f"need a workspace larger than {required_workspace[args.dataset]} for {args.dataset} dataset"
     assert args.optimizer in ["adam", "rmsprop"], "invalid optimizer"
 
     torch.multiprocessing.spawn(train, args=(args,), nprocs=args.num_shards, join=True)

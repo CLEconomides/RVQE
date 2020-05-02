f649317e030bafb900fa8ca519d73faa015a9f94
Johannes Bausch <johannes.bausch@gmail.com>
2020-05-02 11:21:22 +0100

    updated experiment to include lbfgs

--- experiment-1.sh
+++ experiment-1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-optimizers=( "sgd" "rmsprop" "adam" )
+optimizers=( "sgd" "rmsprop" "adam" "lbfgs" )
 learningrates=( 10.0 5.0 2.0 1.0 0.5 0.2 0.1 0.05 0.02 0.01 0.005 0.002 0.001 0.0005 0.0002 0.0001 0.00005 0.00002 0.00001 )
 
 LOCKFILEFOLDER="./locks"

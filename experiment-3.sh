8def79d55c7be1f9788123117ec12c0e79cab7a7
Johannes Bausch <johannes.bausch@gmail.com>
2020-05-02 16:07:50 +0100

    experiment 3 with flexible port

--- experiment-3.sh
+++ experiment-3.sh
@@ -23,7 +23,8 @@ do
                 touch "$LOCKFILE"
                 sync
                 echo "running order $o, stages $s, workspace $w"
-                ./main.py --num-shards 2 --tag experiment3-$o-$s-$w --epochs 500 train --dataset elman-xor --stages $s --workspace $w --order $o --optimizer rmsprop --learning-rate 0.15 --batch-size 1
+                (( port = 16337 + 100 * $w + 10 * $s + $o ))
+                ./main.py --port $port --num-shards 2 --tag experiment3-$o-$s-$w --epochs 500 train --dataset elman-xor --stages $s --workspace $w --order $o --optimizer rmsprop --learning-rate 0.15 --batch-size 1
                 touch "$DONEFILE"
                 sync
                 sleep 1

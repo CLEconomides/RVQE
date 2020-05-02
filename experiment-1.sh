1f9e50b9b08fa4399247811fc5aab54e564f5dcb
Johannes Bausch <johannes.bausch@gmail.com>
2020-05-02 08:40:37 +0100

    better experiment1 scheduling

--- experiment-1.sh
+++ experiment-1.sh
@@ -3,10 +3,25 @@
 optimizers=( "sgd" "rmsprop" "adam" )
 learningrates=( 10.0 5.0 2.0 1.0 0.5 0.2 0.1 0.05 0.02 0.01 0.005 0.002 0.001 0.0005 0.0002 0.0001 0.00005 0.00002 0.00001 )
 
+LOCKFILEFOLDER="./locks"
+mkdir -p "$LOCKFILEFOLDER"
+
 for optim in "${optimizers[@]}"
 do
     for lr in "${learningrates[@]}"
     do
-        ./main.py --tag lr-optim-test --epochs 500 train --optimizer $optim --learning-rate $lr
+        LOCKFILE="$LOCKFILEFOLDER/experiment1-$optim-$lr.lock"
+        DONEFILE="$LOCKFILEFOLDER/experiment1-$optim-$lr.done"
+        if [[ ! -f "$LOCKFILE" && ! -f "$DONEFILE" ]]
+        then
+            touch "$LOCKFILE"
+            echo "running $optim with $lr"
+            ./main.py --tag experiment1-$optim-$lr --epochs 500 train --optimizer $optim --learning-rate $lr
+            touch "$DONEFILE"
+            sleep 1
+            rm "$LOCKFILE"
+        else
+            echo "skipping $optim with $lr"
+        fi
     done
 done

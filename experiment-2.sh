e02d0cd32f9be0d5d38032aadc036692e06007fb
Johannes Bausch <johannes.bausch@gmail.com>
2020-05-05 15:16:14 +0100

    made experiment 2 with seeds

--- experiment-2.sh
+++ experiment-2.sh
@@ -2,31 +2,35 @@
 
 optimizers=( "sgd" "rmsprop" "adam" )
 learningrates=( 10.0 5.0 2.0 1.0 0.5 0.2 0.1 0.05 0.02 0.01 0.005 0.002 0.001 0.0005 0.0002 0.0001 0.00005 0.00002 0.00001 )
+seeds=( 1523 2342 1231 9948 2349 5675 4358 9389 9999 2525)
 
 LOCKFILEFOLDER="./locks"
 mkdir -p "$LOCKFILEFOLDER"
 
 sleep $[ ($RANDOM % 400) + 1 ]s
 
-for optim in "${optimizers[@]}"
+for sd in "${seeds[@]}"
 do
-    for lr in "${learningrates[@]}"
+    for optim in "${optimizers[@]}"
     do
-        LOCKFILE="$LOCKFILEFOLDER/experiment2-$optim-$lr.lock"
-        DONEFILE="$LOCKFILEFOLDER/experiment2-$optim-$lr.done"
-        sync
-        if [[ ! -f "$LOCKFILE" && ! -f "$DONEFILE" ]]
-        then
-            touch "$LOCKFILE"
+        for lr in "${learningrates[@]}"
+        do
+            LOCKFILE="$LOCKFILEFOLDER/experiment2-$seed-$optim-$lr.lock"
+            DONEFILE="$LOCKFILEFOLDER/experiment2-$seed-$optim-$lr.done"
             sync
-            echo "running $optim with $lr"
-            ./main.py --tag experiment2-$optim-$lr --epochs 500 train --dataset elman-xor --stages 4 --optimizer $optim --learning-rate $lr --sentence-length 21 --batch-size 3
-            touch "$DONEFILE"
-            sync
-            sleep 1
-            rm "$LOCKFILE"
-        else
-            echo "skipping $optim with $lr"
-        fi
+            if [[ ! -f "$LOCKFILE" && ! -f "$DONEFILE" ]]
+            then
+                touch "$LOCKFILE"
+                sync
+                echo "running $optim with $lr"
+                ./main.py --tag experiment2-$seed-$optim-$lr --seed $seed --num-shards 3 --epochs 500 train --dataset elman-xor --stages 3 --optimizer $optim --learning-rate $lr --sentence-length 3 --batch-size 4
+                touch "$DONEFILE"
+                sync
+                sleep 1
+                rm "$LOCKFILE"
+            else
+                echo "skipping $optim with $lr and seed $seed"
+            fi
+        done
     done
 done

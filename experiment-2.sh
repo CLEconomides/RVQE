63b4c9066905c69c12de9f017ea91d4e81f75b23
Johannes Bausch <johannes.bausch@gmail.com>
2020-05-05 15:26:41 +0100

    changed grid search order for experiment 2

--- experiment-2.sh
+++ experiment-2.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 optimizers=( "sgd" "rmsprop" "adam" )
-learningrates=( 10.0 5.0 2.0 1.0 0.5 0.2 0.1 0.05 0.02 0.01 0.005 0.002 0.001 0.0005 0.0002 0.0001 0.00005 0.00002 0.00001 )
-seeds=( 1523 2342 1231 9948 2349 5675 4358 9389 9999 2525)
+learningrates=( 10.0 5.0 2.0 1.0 0.5 0.2 0.1 0.05 0.02 0.01 0.005 0.002 0.001 0.0005 0.0002 0.0001 )
+seeds=( 1523 2342 1231 9948 2349 5675 4358 9389 9999 2525 )
 
 LOCKFILEFOLDER="./locks"
 mkdir -p "$LOCKFILEFOLDER"
@@ -11,9 +11,9 @@ sleep $[ ($RANDOM % 400) + 1 ]s
 
 for sd in "${seeds[@]}"
 do
-    for optim in "${optimizers[@]}"
+    for lr in "${learningrates[@]}"
     do
-        for lr in "${learningrates[@]}"
+        for optim in "${optimizers[@]}"
         do
             LOCKFILE="$LOCKFILEFOLDER/experiment2-$seed-$optim-$lr.lock"
             DONEFILE="$LOCKFILEFOLDER/experiment2-$seed-$optim-$lr.done"
@@ -23,7 +23,7 @@ do
                 touch "$LOCKFILE"
                 sync
                 echo "running $optim with $lr"
-                ./main.py --tag experiment2-$seed-$optim-$lr --seed $seed --num-shards 3 --epochs 500 train --dataset elman-xor --stages 3 --optimizer $optim --learning-rate $lr --sentence-length 3 --batch-size 4
+                ./main.py --tag experiment2-$seed-$optim-$lr --seed $sd --num-shards 3 --epochs 500 train --dataset elman-xor --stages 3 --optimizer $optim --learning-rate $lr --sentence-length 3 --batch-size 4
                 touch "$DONEFILE"
                 sync
                 sleep 1

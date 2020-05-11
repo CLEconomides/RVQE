28b72a62fee797d083da5a76d48352846747ab48
Johannes Bausch <johannes.bausch@gmail.com>
2020-05-11 10:35:50 +0100

    amended experiment 2

--- experiment-2.sh
+++ experiment-2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # see whether we can get the elman-xor setup to converge
 
-batchsizes=( 5 )
+batchsizes=( 2 )
 learningrates=( 0.005 0.02 )
 seeds=( 720 7292 4402 5427 4269 7928 3475 5114 3975 2733 1217 8443 2 2826 9432 6936 5081 3774 7427 700 1664 7262 499 9736 6654 )
 
@@ -30,11 +30,11 @@ do
                         ./main.py \
                             --tag experiment2-$TAG \
                             --seed $sd \
-                            --num-shards 1 \
+                            --num-shards 3 \
                             --epochs 1000 \
                             train \
                             --dataset simple-quotes \
-                            --workspace 8 \
+                            --workspace 7 \
                             --stages 3 \
                             --order 2 \
                             --degree 4 \
